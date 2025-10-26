702c1cd8254a5093c5d7e34fae17dde01c35d2c3
dbenasco <59152982+dbenasco@users.noreply.github.com>
2025-10-26 16:16:55 +0100

    Letras mas grandes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './Header.css';
 
 function Header({ title }) {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  //  ESTO NO FUNCIONAAAA PARA CHATS
+  const handleBack = () => {
+     if (location.pathname.startsWith("/chat/") && location.pathname !== "/chat") {
+      navigate("/chat", { replace: true });
+      return;
+    } else {
+      navigate(-1); 
+    }
+  };
 
   return (
     <header className="new-app-header">
-      <button onClick={() => navigate(-1)} className="header-btn back-btn">
+       <button onClick={handleBack} className="header-btn back-btn">
         <span>&larr;</span> Atrás
       </button>
-      <h1 className="header-title">{title}</h1>
+       <h1 className="header-title">{title}</h1>
+
       <button onClick={() => navigate('/')} className="header-btn home-btn">
         <span>&#8962;</span> Inicio
       </button>
     </header>
   );
 }
+
 
 export default Header;
